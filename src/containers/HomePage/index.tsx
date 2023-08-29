420a60bee39a68d8e56d748a28b9b92c0f48c4c5
SavioRC <savio_rc@hotmail.com.br>
2023-08-29 16:50:46 -0300

    borders

--- src/containers/HomePage/index.tsx
+++ src/containers/HomePage/index.tsx
@@ -5,11 +5,11 @@ export default function HomePage({ posts }: HomeProps) {
   return (
     <HomeContainer>
       {posts.map((post) => (
-        <div className="m-auto" key={post.attributes.slug}>
-          <h2 className="py-4">{post.attributes.title}</h2>
+        <div className="m-auto hover:border-secondary hover:border-opacity-75" key={post.attributes.slug}>
+          <h2 className="py-4 flex items-center justify-center">{post.attributes.title}</h2>
           <div>
             <img
-              className="h-40 w-64"
+              className="h-40 w-64 hover:cursor-pointer"
               src={post.attributes.cover.data.attributes.url}
               alt={post.attributes.cover.data.attributes.name}
             />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,10 +10,12 @@ export default function Home({ posts }: HomeProps) {
     <>
       <Header></Header>
       <HomePage posts={posts}></HomePage>
-      {/* <img
-        className="h-40 w-64 object-cover"
-        src="	https://res.cloudinary.com/dlaaz49my/image/upload/v1692836547/js_simple_1e52b9a734.png"
-      /> */}
+      {/* <div className="flex justify-center border-8 border-secondary">
+        <img
+          className="h-40 w-64 hover:cursor-pointer"
+          src="	https://res.cloudinary.com/dlaaz49my/image/upload/v1692836547/js_simple_1e52b9a734.png"
+        />
+      </div> */}
     </>
   );
 }
