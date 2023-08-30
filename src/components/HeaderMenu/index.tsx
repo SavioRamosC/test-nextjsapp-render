fe6efc00ed6133978879ce14da49b4d4c2b86d99
SavioRC <savio_rc@hotmail.com.br>
2023-08-30 16:51:36 -0300

    30-08-2023

--- src/components/HeaderMenu/index.tsx
+++ src/components/HeaderMenu/index.tsx
@@ -8,12 +8,10 @@ export default function HeaderMenu({ isOpen }: MenuOptionsProps) {
   }
 
   return (
-    <div className="h-1/4w-1/6">
-      <ul className="absolute top-8 right-0 bg-tertiary border border-gray-300 rounded-md shadow-md">
-        <li className="py-2 px-4 hover:bg-secondary rounded-t-md">Option 1</li>
-        <li className="py-2 px-4 hover:bg-secondary ">Option 2</li>
-        <li className="py-2 px-4 hover:bg-secondary rounded-b-md">Option 3</li>
-      </ul>
-    </div>
+    <ul className="w-64 flex flex-col absolute top-8 right-0 bg-tertiary border border-gray-300 rounded-md shadow-md">
+      <li className="py-2 px-4 hover:bg-secondary rounded-t-md">My Account</li>
+      <li className="py-2 px-4 hover:bg-secondary ">Privacy</li>
+      <li className="py-2 px-4 hover:bg-secondary rounded-b-md">Settings</li>
+    </ul>
   );
 }
