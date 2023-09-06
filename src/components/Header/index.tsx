8d8a896855299e58657cc104305fdcbf9f057d1f
SavioRC <savio_rc@hotmail.com.br>
2023-09-06 15:29:03 -0300

    06-09-2023

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -11,24 +11,24 @@ export default function Header() {
 
   return (
     <header className="fixed w-full bg-primary text-white py-3">
-      <ul className="flex">
-        <li className="ml-auto transition duration-300 hover:bg-secondary rounded-lg">
+      <ul className="flex justify-center items-center">
+        <li className="md:ml-auto transition duration-300 hover:bg-secondary rounded-lg">
           <Link className="w-full h-full flex py-2 px-12 rounded-lg" href="/">
             Home
           </Link>
         </li>
-        <li className="m-auto transition duration-300 hover:bg-secondary rounded-lg">
+        <li className="md:m-auto transition duration-300 hover:bg-secondary rounded-lg">
           <Link className="w-full h-full flex py-2 px-12 rounded-lg" href="/">
             Posts
           </Link>
         </li>
-        <li className="mr-auto transition duration-300 hover:bg-secondary rounded-lg">
+        <li className="md:mr-auto transition duration-300 hover:bg-secondary rounded-lg">
           <Link className="w-full h-full flex py-2 px-12 rounded-lg" href="/about">
             About
           </Link>
         </li>
         <li
-          className="top-3 right-6 absolute transition duration-300 hover:bg-secondary py-2 px-2 rounded-lg cursor-pointer"
+          className="md:top-3 md:right-6 md:absolute transition duration-300 hover:bg-secondary py-2 px-2 rounded-lg cursor-pointer"
           onClick={toggleMenu}
         >
           ☰
