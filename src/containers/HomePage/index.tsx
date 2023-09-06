9c688c1ffbd9033d1ade531a5d3f1163e0550c46
SavioRC <savio_rc@hotmail.com.br>
2023-09-06 15:04:05 -0300

    06-09-2023

--- src/containers/HomePage/index.tsx
+++ src/containers/HomePage/index.tsx
@@ -1,23 +1,25 @@
 import HomeContainer from '@/components/HomeContainer/index';
 import { PostArrayProps } from '@/types/post-props';
 import { modifyImageSize } from '@/utils/modify-image-size';
-import Image from 'next/image';
 import Link from 'next/link';
 
 export default function HomePage({ posts }: PostArrayProps) {
   return (
     <HomeContainer>
       {posts.map((post) => (
-        <Link className="m-auto hover:opacity-50" key={post.attributes.slug} href={`/post/${post.attributes.slug}`}>
-          <h2 className="py-4 text-center md:text-left flex items-center justify-center md:justify-start group-hover:cursor-pointer">
-            {post.attributes.title}
-          </h2>
-          <Image
-            width={320}
-            height={180}
-            src={modifyImageSize(post.attributes.cover.data.attributes.url, 320, 180)}
-            alt={post.attributes.cover.data.attributes.name}
-          />
+        <Link className="m-auto group" key={post.attributes.slug} href={`/post/${post.attributes.slug}`}>
+          <div className="relative">
+            <h2 className="py-4 text-center md:text-left flex items-center justify-center md:justify-start group-hover:cursor-pointer">
+              {post.attributes.title}
+            </h2>
+            <img
+              className="hover:cursor-pointer transition-transform transform hover:scale-110 hover:rotate-5"
+              width={320}
+              height={180}
+              src={modifyImageSize(post.attributes.cover.data.attributes.url, 320, 180)}
+              alt={post.attributes.cover.data.attributes.name}
+            />
+          </div>
         </Link>
       ))}
     </HomeContainer>
