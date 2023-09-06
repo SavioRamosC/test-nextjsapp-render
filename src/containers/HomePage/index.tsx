3602ae80877087c9983d59f6fb00e65ffb2ccfe5
SavioRC <savio_rc@hotmail.com.br>
2023-09-06 15:54:51 -0300

    06-09-2023

--- src/containers/HomePage/index.tsx
+++ src/containers/HomePage/index.tsx
@@ -1,6 +1,7 @@
 import HomeContainer from '@/components/HomeContainer/index';
 import { PostArrayProps } from '@/types/post-props';
 import { modifyImageSize } from '@/utils/modify-image-size';
+import Image from 'next/image';
 import Link from 'next/link';
 
 export default function HomePage({ posts }: PostArrayProps) {
@@ -12,7 +13,7 @@ export default function HomePage({ posts }: PostArrayProps) {
             <h2 className="py-4 text-center md:text-left flex items-center justify-center md:justify-start group-hover:cursor-pointer">
               {post.attributes.title}
             </h2>
-            <img
+            <Image
               className="hover:cursor-pointer transition-transform transform hover:scale-110 hover:rotate-5"
               width={320}
               height={180}
