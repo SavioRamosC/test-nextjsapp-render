6df06ff38736dc6c48663d8cbf4880d890b6e078
SavioRC <savio_rc@hotmail.com.br>
2023-09-13 15:36:30 -0300

    13-09-2023

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,22 +5,17 @@ import { getAllPosts } from '@/data/posts/get-all-posts';
 import { GetStaticProps } from 'next';
 import { PostArrayProps } from '@/types/post-props';
 import Footer from '@/components/Footer';
+import Head from 'next/head';
 
 export default function Home({ posts }: PostArrayProps) {
   return (
     <div className="min-h-screen">
+      <Head>
+        <title>{`${process.env.SITE_NAME}`}</title>
+      </Head>
       <Header></Header>
       <HomePage posts={posts}></HomePage>
       <Footer></Footer>
-      {/* <div className="flex justify-center group">
-        <h2 className="py-4 flex items-center justify-center transition-all duration-1000 group-hover:text-secondary">
-          haar
-        </h2>
-        <img
-          className="h-40 w-64 border-solid border-secondary hover:cursor-pointer hover:border-2 border-opacity-5"
-          src="	https://res.cloudinary.com/dlaaz49my/image/upload/v1692836547/js_simple_1e52b9a734.png"
-        />
-      </div> */}
     </div>
   );
 }
